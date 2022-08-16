5ab8aa82eac74e893dbf1b68ef74e3ba81e29111
“rushdy87” <48268026+m7md7sam@users.noreply.github.com>
2022-08-16 16:22:06 +0300

    maxChar: return the character that is most commonly used in the string.

---
+++ 04maxChar/index.js
@@ -0,0 +1,46 @@
+// --- Directions
+// Given a string, return the character that is most
+// commonly used in the string.
+// --- Examples
+// maxChar("abcccccccd") === "c"
+// maxChar("apple 1231111") === "1"
+
+function maxChar(str) {
+  const chars = {};
+  for (let char of str) {
+    chars[char] = chars[char] + 1 || 1;
+  }
+  let max = str[0];
+  for (let char in chars) {
+    max = chars[char] > chars[max] ? char : max;
+  }
+  return max;
+}
+
+module.exports = maxChar;
+
+/*
+Solution:
+function maxChar(str) {
+  const charMap = {};
+  let max = 0;
+  let maxChar = '';
+
+  for (let char of str) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  }
+
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
+}
+*/
