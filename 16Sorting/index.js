bbdb7a5af56719b4a876053900a02ca781b97004
“rushdy87” <48268026+m7md7sam@users.noreply.github.com>
2022-09-06 07:50:26 +0300

    Sorting Algorithms: Bubble Sort Algorithm

---
+++ 16Sorting/index.js
@@ -0,0 +1,39 @@
+// --- Directions
+// Implement bubbleSort, selectionSort, and mergeSort
+
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const lesser = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = lesser;
+      }
+    }
+  }
+  return arr;
+}
+
+function selectionSort(arr) {}
+
+function mergeSort(arr) {}
+
+function merge(left, right) {}
+
+module.exports = { bubbleSort, selectionSort, mergeSort, merge };
+
+/**
+ * bubble Sort
+ function bubbleSort(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[i]) {
+                let tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
+            }
+        }
+    }
+    return arr;
+}
+ */
